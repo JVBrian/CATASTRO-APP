bdd38b8f35303354f3d1ddaab30dfe0a7af16562
jvbrian <brianjarabav@hotmail.com>
2021-12-02 22:35:31 -0500

    cambios en la pagina principal

--- client/src/components/body/home/Home.js
+++ client/src/components/body/home/Home.js
@@ -4,20 +4,75 @@ import './home.css'
 function Home() {
     return (
         <div className="home_page">
-            <h2>Bienvenido a su plataforma catastral</h2>
+            <h1>Bienvenido a su plataforma catastral</h1>
+            <br></br>
+            <img className= "home_img" src="../../leticia.jpg"></img>
+            
+            <h2>¿ Qué es ?</h2>
+            <br></br>
+
             <p>
-                Este sitio web trata de una empresa 
-                que cobra el catastro de los usuarios,
-                es un proyecto que está siendo trabajado
-                por el grupo 8.
+                Es el impuesto que recae sobre la propiedad inmueble y se genera 
+                por la existencia del predio. Su base gravable depende del avalúo 
+                catastral. Es una renta endógena, de propiedad de los municipios y 
+                distritos, quienes tienen a su cargo su administración, recaudo y 
+                control. Esto permite la concesión de exenciones tributarias, y la 
+                adopción de tarifas preferenciales con fines extrafiscales propios 
+                de los impuestos.
             </p>
+            
+            <br></br>
+            <br></br>
+            <h2>¿Para qué es?</h2>
+            <br></br>
+            
+            <ul>
+                <li type="disc">
+                    Puede ser un poderoso mecanismo de distribución de la riqueza, a 
+                    partir del cobro progresivo sobre una base gravable y tarifas 
+                    actualizadas.
+                </li>
+                <br></br>
+                <li >
+                    Constituye una de las principales rentas de los municipios y distritos, 
+                    y una parte significativa de sus ingresos corrientes propios.
+
+                </li>
+                <br></br>
+                <li >
+                    Permite usos extrafiscales, como los disuasorios y de incentivos, para 
+                    el ordenamiento territorial: por ejemplo, a partir de exenciones y 
+                    tarifas preferenciales, los municipios y distritos pueden impulsar o 
+                    estimular determinadas actividades que resulten meritorias o necesarias.
+
+                </li>
+                <br></br>
+                <li >
+                    Puede ser un poderoso mecanismo de distribución de la riqueza, a 
+                    partir del cobro progresivo sobre una base gravable y tarifas 
+                    actualizadas.
+
+                </li>
+                <br></br>
+                <li >
+                    Mediante la adopción de tarifas más altas se puede desincentivar 
+                    las actividades de determinados predios con externalidades 
+                    negativas, como en el caso de los predios urbanizados sin edificar, 
+                    urbanizables no urbanizados, o actividades de fuerte impacto para el 
+                    municipio o distrito.
+
+                </li>
+            
+            
+            </ul>
+          
             <a href="https://www.youtube.com/watch?v=dkNfNR1WYMY" target="_blank" 
-            rel="noopener noreferrer">Aquí debemos poner algo</a>
+            rel="noopener noreferrer">Más información</a>
 
-            <h3>Acá debemos colocar otra cosa (Está en proceso esta vaina)</h3>
-
+            
+            
             <a href="https://www.youtube.com/watch?v=8SbUC-UaAxE" target="_blank" 
-            rel="noopener noreferrer">Boton para agregar alguna maricada</a>
+            rel="noopener noreferrer">Enlaces de interés</a>
         </div>
     )
 }
